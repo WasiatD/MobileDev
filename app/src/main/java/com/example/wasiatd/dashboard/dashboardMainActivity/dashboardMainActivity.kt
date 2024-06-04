36a37bbd2dc1d8a55819bedb179b390a2b7b51b7
Udin <mafifudinarsyada@gmail.com>
2024-06-04 10:03:52 +0700

    add prototype view for task, plant, search fragment. Add dummy data for dashboard

--- app/src/main/java/com/example/wasiatd/dashboard/dashboardMainActivity/dashboardMainActivity.kt
+++ app/src/main/java/com/example/wasiatd/dashboard/dashboardMainActivity/dashboardMainActivity.kt
@@ -1,15 +1,19 @@
 package com.example.wasiatd.dashboard.dashboardMainActivity
 
+import DashboardAdapter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentTransaction
 import androidx.fragment.app.commit
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.wasiatd.R
 import com.example.wasiatd.fragments.HomeFragment
 import com.example.wasiatd.fragments.PlantFragment
 import com.example.wasiatd.fragments.TasksFragment
 import com.example.wasiatd.fragments.SearchFragment
+import com.example.wasiatd.utils.ItemDataDashboard
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class dashboardMainActivity : AppCompatActivity() {
@@ -22,6 +26,9 @@ class dashboardMainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard_main)
+
+        // Inside onCreateView() or onViewCreated() of each fragment
+
 
         supportFragmentManager.beginTransaction().apply {
             add(R.id.fragment_container, searchFragment, "4")
@@ -54,13 +61,19 @@ class dashboardMainActivity : AppCompatActivity() {
                     true
                 }
                 R.id.navigation_tasks -> {
-                    supportFragmentManager.beginTransaction().hide(activeFragment).show(tasksFragment).commit()
-                    activeFragment = tasksFragment
+                    val tasksFragment = TasksFragment.newInstance("param1", "param2")
+                    supportFragmentManager.commit {
+                        replace(R.id.fragment_container, tasksFragment)
+                        addToBackStack(null) // Optional: Add transaction to back stack
+                    }
                     true
                 }
                 R.id.navigation_search -> {
-                    supportFragmentManager.beginTransaction().hide(activeFragment).show(searchFragment).commit()
-                    activeFragment = searchFragment
+                    val searchFragment = SearchFragment.newInstance("param1", "param2")
+                    supportFragmentManager.commit {
+                        replace(R.id.fragment_container, searchFragment)
+                        addToBackStack(null) // Optional: Add transaction to back stack
+                    }
                     true
                 }
                 else -> false
