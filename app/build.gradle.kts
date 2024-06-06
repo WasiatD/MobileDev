2556b430b8c725c89f8eb61516005692bf852c31
akmalariq9 <akmalariq009@gmail.com>
2024-06-06 22:35:12 +0700

    feat: add plant details page

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,11 +51,7 @@ dependencies {
     implementation ("com.github.bumptech.glide:glide:4.12.0")
     annotationProcessor ("com.github.bumptech.glide:compiler:4.12.0")
     implementation ("androidx.activity:activity-ktx:1.2.0")
-
-
-
-
-
+    implementation("com.github.bumptech.glide:glide:4.16.0")
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
